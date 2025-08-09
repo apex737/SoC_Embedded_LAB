b856d8cd38019fbea22e5b9aad609a30be6f8db7
apex737 <124483183+apex737@users.noreply.github.com>
2025-08-09 15:47:07 +0900

    ;

--- fifo/sync_fifo.md
+++ fifo/sync_fifo.md
@@ -12,3 +12,8 @@
 - 데이터를 A, B 채널로 나눠서 처리한 뒤 합치는 경우 Latency 차이에 의해 더 빠른 쪽(B 채널)이 멈추는 상황이 발생함
 - 채널 하나가 막히면 Split-Input이 멈추고, 이로 인해 새로운 입력도 멈추는 상황이 발생함 **(Deadlock)**
 - 이 문제는 채널 간 **Latency 차이만큼 FIFO DEPTH를 맞춰주는 것으로 해결 가능**
+
+#### 3. Configuration
+
+- Strobe (full, empty)
+- Round (Circulate Flag)
--- fifo/sync_fifo.v
+++ fifo/sync_fifo.v
@@ -13,42 +13,70 @@ module sync_fifo
 	// master side
 	output m_valid,
 	input m_ready,
-	output [SIZE-1:0] m_data,
-	// strobe
-	// input push, pop, 
-	output full, empty
+	output [SIZE-1:0] m_data
 );
 
 reg [SIZE-1:0] fifo [0:DEPTH-1];
-reg [LOG2DEPTH-1] wptr, rptr;
-reg wptr_round, rptr_round;
-
-assign full =  (wptr_round != rptr_round) && (wptr == rptr);
-assign empty = (wptr_round == rptr_round) && (wptr == rptr);
+// Next 포인터는 조합논리, Current 포인터는 순차논리로 분리
+reg [LOG2DEPTH-1:0] wptr, wptr_nxt, rptr, rptr_nxt;
+reg [LOG2DEPTH-1:0] wptr_round, wptr_round_nxt, rptr_round, rptr_round_nxt;
 
 integer i;
-
 // Write Side
+// Write: Sequential Logic
 wire w_hs = s_valid & s_ready;
 always@(posedge clk or negedge rstn) begin
 	if(~rstn) begin
 		for(i=0;i<DEPTH;i=i+1) fifo[i] <= 0;
 		wptr <= 0;
-	end else if(w_hs && ~full) begin
+		wptr_round <= 0;
+	end else if(w_hs) begin
 		fifo[wptr] <= s_data;
-		wptr = wptr + 1;
+		{wptr, wptr_round} <= {wptr_nxt, wptr_round_nxt};
 	end
 end
 
-// update round flag : 한바퀴 돌면 반전
-// 1. 한바퀴 돈거를 어떻게 표현하나?
-// 2. 현재상태를 유지해야하니 결국 래치를 만드나
-// 3. next로 굳이 조합논리와 순차논리를 구분하여 관리하는 이유가 래치를 피하려고 그런가?
+// Write: Next State (Combinational) Logic
+// to avoid Latch & Combinational Loop
 always@* begin
-	if(wptr == DEPTH-1) wptr_round = ~wptr_round;
-	else wptr_round = wptr_round;
+	if(wptr == DEPTH-1) begin 
+		wptr_round_nxt = ~wptr_round;
+		wptr_nxt = 0;
+	end else begin 
+		wptr_round_nxt = wptr_round;
+		wptr_nxt = wptr + 1;
+	end
 end
 
 // Read Side
+// Read: Sequential Logic
+wire r_hs = m_valid & m_ready;
+always@(posedge clk or negedge rstn) begin
+	if(~rstn) begin
+		rptr <= 0;
+		rptr_round <= 0;
+	end else if(r_hs) begin
+		{rptr, rptr_round} <= {rptr_nxt, rptr_round_nxt};
+	end
+end
+
+// Read: Next State (Combinational) Logic
+// to avoid Latch & Combinational Loop
+always@* begin
+	if(rptr == DEPTH-1) begin 
+		rptr_round_nxt = ~rptr_round;
+		rptr_nxt = 0;
+	end else begin 
+		rptr_round_nxt = rptr_round;
+		rptr_nxt = rptr + 1;
+	end
+end
+
+assign m_data = fifo[rptr];
+assign full =  (wptr_round != rptr_round) && (wptr == rptr);
+assign empty = (wptr_round == rptr_round) && (wptr == rptr);
+assign s_ready = ~full;
+assign m_valid = ~empty;
 
 endmodule
+
