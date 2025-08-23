ef020eb511057172227c3e49c84a4703a8b59901
apex737 <apex737@naver.com>
2025-08-23 20:37:04 +0900

    ;

--- DSP/Filter1D/spectrum.m
+++ DSP/Filter1D/spectrum.m
@@ -5,7 +5,7 @@ f  = (0:n-1)*(Fs/n);      % 분해능 Δf = Fs/n인 주파수 벡터
 yf = fft(y);              % FFT 결과 (복소수)
 fp = abs(yf).^2/n;        % 파워 스펙트럼 (정규화) = |Y(f)|² / n
 
-set(0,'defaultfigurevisible','off');      % 창 안 띄움(WSL 안정)
+set(0,'defaultfigurevisible','off');      % headless
 graphics_toolkit gnuplot;                 % gnuplot로 렌더
 plot(f, 10*log10(fp));                    % dB 스케일 플롯
 xlabel('Frequency (Hz)');
